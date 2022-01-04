6413b54a953bdfeff49d6ca112a6599ac02f1053
Ashiyooo <76894491+Ashiyooo@users.noreply.github.com>
2022-01-04 10:01:41 +0800

    Add files via upload

---
+++ Sprint 3/Backend-Database/src/others/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from operator import attrgetter
+from blog.models import BlogPost
+# Create your views here.
+def home_screen_view(request):
+	
+	context = {}
+	blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+	context['blog_posts'] = blog_posts
+	
+	return render(request, "others/home.html", context)
